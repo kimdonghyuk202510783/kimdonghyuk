b4b34070712b1827f8e4a95a9e1c95a183f05f3d
kimdonghyuk202510783 <kkmh0456@gmail.com>
2025-04-24 19:00:05 +0900

    Add files via upload

---
+++ 7주차 과제.py
@@ -0,0 +1,19 @@
+def dan():
+    for j in range(1, 10):
+        print(f'2 x {j} = {2*j:<3}', end='  ')
+        print(f'3 x {j} = {3*j:<3}', end='  ')
+        print(f'4 x {j} = {4*j:<3}', end='  ')
+        print(f'5 x {j} = {5*j:<3}')
+
+    print()  
+
+    for j in range(1, 10):
+        print(f'6 x {j} = {6*j:<2}', end='   ')
+        print(f'7 x {j} = {7*j:<2}', end='   ')
+        print(f'8 x {j} = {8*j:<2}', end='   ')
+        print(f'9 x {j} = {9*j:<2}')
+
+dan()
+
+    
+
